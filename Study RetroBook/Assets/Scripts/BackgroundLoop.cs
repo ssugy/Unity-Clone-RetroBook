4aba85dfb12777e39b2a20633ba003133fc4a64c
YHKwon <ssugy2@gmail.com>
2022-08-03 00:13:02 +0900

    #1 스터디용 자료 준비중

--- Study RetroBook/Assets/Scripts/BackgroundLoop.cs
+++ Study RetroBook/Assets/Scripts/BackgroundLoop.cs
@@ -6,14 +6,25 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Awake() {
         // 가로 길이를 측정하는 처리
+        // 아니 가로길이를 왜 콜라이더 써서 구하는거지? 그냥 sprite상태에서 못구하나?
+        // 구할수있음. 이런데 쓰라고 SpriteRenderer에 size프로퍼티가 붙어있는 것임.
+        print(GetComponent<SpriteRenderer>().size);
+        width = GetComponent<SpriteRenderer>().size.x;
     }
 
     private void Update() {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+        if (transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        
+        // 현재 위치에서 오른쪽으로 가로길이 * 2 만큼 이동 - 이미지 2개 이어붙일거라서 괜찮음.
+        // 굳이 형변환말고 vecto3로 진행.
+        Vector3 offset = new Vector3(width * 2, 0, 0);
+        transform.position += offset;
     }
 }
--- Study RetroBook/Assets/Scripts/ScrollingObject.cs
+++ Study RetroBook/Assets/Scripts/ScrollingObject.cs
@@ -5,6 +5,7 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        // 초당 speed의 속도로 왼쪽으로 평행이동
+        transform.Translate(Vector3.left * Time.deltaTime);
     }
 }
